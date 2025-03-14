c28ec73858a36f28188c4f7ee4026a0a4556803a
Prabal Kumar Das <prabalkumardas@Prabals-MacBook-Air.local>
2025-03-14 11:44:11 +0530

    Added the hero banner carousel for Fashion Page

--- task/components/home/FashionTab.js
+++ task/components/home/FashionTab.js
@@ -1,9 +1,11 @@
+import HeroBanner from "@/app/components/CustomComponents/HeroBannerCarousel";
 const FashionTabContent = () => {
 
     return (
       <div className="p-6">
         <h2 className="text-lg font-bold mb-3 text-black">Fashion Tab</h2>
         <p className="text-gray-600">Here goes the content for the Fashion tab.</p>
+        <HeroBanner />
       </div>
     );
   };
