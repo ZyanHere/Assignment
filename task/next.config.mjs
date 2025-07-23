77eeb2040fa91da104177734a7814664b33c8c76
AayushSomwanshi21 <aayushsomwanshi21@gmail.com>
2025-07-23 19:29:04 +0530

    Location and UI fix in Events and Hotels page

--- task/next.config.mjs
+++ task/next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     images: {
+  images: {
     domains: [
       'lastminutedeal.s3.ap-southeast-2.amazonaws.com',
       'example.com',
-      'lh3.googleusercontent.com'
+      'lh3.googleusercontent.com',
+      'cdn.shopify.com'
       // any other hosts you fetch images from…
     ],
     // alternatively, you can use `remotePatterns` for more control:
@@ -17,7 +18,7 @@ const nextConfig = {
     //   },
     // ],
   },
-//     ignoreBuildErrors: true,
+  //     ignoreBuildErrors: true,
 
 };
 
